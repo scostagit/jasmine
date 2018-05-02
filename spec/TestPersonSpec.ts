62a93fa3d6581c561d73d73797209245e0b73ad1
SLAINTE\scosta <scosta@vitrosotware.com>
2018-05-02 10:02:28 +0100

    Agora vai

---
+++ spec/TestPersonSpec.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/index.d.ts" />
+
+describe("my first test",()=>{
+    it("my first expectation", ()=>{
+        expect(true).toBeTruthy("What? why was true not truthy?");
+    });
+});
+
---
+++ src/Person.ts
@@ -0,0 +1,11 @@
+export class Person{
+    ID:number;
+    Name:string;
+    /**
+     *
+     */
+    constructor(id:number, name:string) {
+        this.ID = id;
+        this.Name = name;
+    }
+}
